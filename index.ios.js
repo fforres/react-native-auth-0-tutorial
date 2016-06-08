0af52da8161734f1e23d0cd93be483676580f39e
Felipe Torres (fforres) <felipe.torressepulveda@gmail.com>
2016-06-08 11:19:05 +0200

    finishing chapter 3

--- index.ios.js
+++ index.ios.js
@@ -3,28 +3,61 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  TouchableHighlight,
 } from 'react-native';
-
 import Login from './src/components/Login';
-
-
 class reactnativeauth0tutorial extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: '', password: ''};
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Login/>
+        <Text>
+           Usuario
+        </Text>
+        <TextInput
+          style={{
+            height: 40,
+            borderWidth: 1
+          }}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text>
+           Contraseña
+        </Text>
+        <TextInput
+          secureTextEntry
+          style={{
+            height: 40,
+            borderWidth: 1
+          }}
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+        />
+        <TouchableHighlight
+          onPress={() => {
+             alert(JSON.stringify(this.state))
+          }}
+        >
+          <Text>
+            Submit!
+          </Text>
+        </TouchableHighlight>
+
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -32,16 +65,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
-
 AppRegistry.registerComponent('reactnativeauth0tutorial', () => reactnativeauth0tutorial);
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
 } from 'react-native';
 
-export default class Login extends Component {
+
+
+export default React.createClass({
+  getInitialState: function() {
+    return {
+      text: '',
+      password: '',
+    };
+  },
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.saludo}>
-          Hola!
+        <Text>
+           Usuario
         </Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text>
+           Contraseña
+        </Text>
+        <TextInput
+          secureTextEntry
+          style={styles.textInput}
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+        />
       </View>
     );
   }
-}
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -24,8 +48,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  saludo: {
-    fontSize: 40,
-    textAlign: 'center'
+  textInput: {
+    width: 300,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
   }
 });
